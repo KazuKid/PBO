7902a08a3a2c46ed2ec9658eef7f451e323a339a
KazuKid <90496879+KazuKid@users.noreply.github.com>
2023-03-09 20:38:05 +0700

    Create minggu1(poin E-2-C).java

---
+++ minggu1(poin E-2-C).java
@@ -0,0 +1,13 @@
+package tabung;
+
+public class VolumeTabung {
+
+	public static void main(String[] args) {
+		double x = 0.5 * 5;
+		int y = 10;
+		double z = x * y;
+		System.out.println(z);
+
+	}
+
+}
