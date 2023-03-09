f18ad7a097a01b6ee7c381b4de1a4b70e4289cda
KazuKid <90496879+KazuKid@users.noreply.github.com>
2023-03-09 20:04:49 +0700

    Create minggu1(poin E 1).java

---
+++ minggu1(poin E 1).java
@@ -0,0 +1,15 @@
+package test1;
+
+
+public class Helloword {
+
+	public static void main(String[] args) {
+		System.out.println("Halo, Nama Saya Mohammad Rivaldy Yusup");
+		System.out.println("Saya adalah mahasiswa Teknik Informatika Universitas Dian Nuswantoro");
+		System.out.println("Saya sedang Menulis Program Java");
+		
+
+	}
+
+}
+
