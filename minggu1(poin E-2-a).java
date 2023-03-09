d2f9591eac496abb54a3afb88e388deacfea5e39
KazuKid <90496879+KazuKid@users.noreply.github.com>
2023-03-09 20:18:56 +0700

    Create minggu1(poin E-2-a).java

---
+++ minggu1(poin E-2-a).java
@@ -0,0 +1,12 @@
+package KelilingLingkaran;
+
+public class RumusKelLingkarang {
+
+	public static void main(String[] args) {
+		int x = 10;
+		double y = 3.14;
+		double z = x * y;
+		System.out.println(z);
+	}
+
+}
