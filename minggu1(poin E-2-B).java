b027b9b52c1a6fff764330c9dafc01b7594032ad
KazuKid <90496879+KazuKid@users.noreply.github.com>
2023-03-09 20:24:43 +0700

    Create minggu1(poin E-2-B).java

---
+++ minggu1(poin E-2-B).java
@@ -0,0 +1,14 @@
+package Segitiga;
+
+public class RumusSegitigaSikuSiku {
+
+	public static void main(String[] args) {
+		double x = 0.5;
+		int y = 6;
+		int z = 8;
+		double h = x * y * z;
+		System.out.println(h);
+
+	}
+
+}
